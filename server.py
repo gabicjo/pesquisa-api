7ab343ba2e61b337e2f007f736e1009fb54caa43
gabicjo <gabriebs@proton.me>
2026-03-26 15:25:38 -0300

    feat: verify json content

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ CORS(app)
 @app.route('/enviar_dados', methods=['POST'])
 def receber_dados():
     dados = request.json
+
+    if not dados:
+        return jsonify({"erro": "JSON vazio ou não enviado"}), 400
+
     print(dados)
 
     return jsonify({"dados_recebidos": dados}), 201
